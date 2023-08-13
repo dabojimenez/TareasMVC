d1885c2ed6237f8d8c1b9ee4c430f01b607dfeab
David Jimenez <dabojimenez16@gmail.com>
2023-08-13 15:46:00 -0500

    se agrega mediante una migracion el rol de admin en la base de datos

---
+++ Migrations/20230813204310_AdminRol.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"if not exists(select id from AspNetRoles where id = 'd24e6049-c2aa-48c8-bf60-239160234628')
+                                        begin
+	                                        insert AspNetRoles (id, Name, NormalizedName)
+	                                        values ('d24e6049-c2aa-48c8-bf60-239160234628', 'admin', 'ADMIN')
+                                        end");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"delete from AspNetRoles where id = 'd24e6049-c2aa-48c8-bf60-239160234628'");
+        }
+    }
+}
