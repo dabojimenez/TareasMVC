e793bfd3c65f5561bd3dafe0c91e4386947033b8
sjimenezprocelec <sjimenez@procelec.ec>
2023-09-15 11:56:13 -0500

    boton para agregar una nueva tarea, de forma temporal usando JavaScript, NO VALIO pero se envia

---
+++ wwwroot/js/tareas.js
@@ -0,0 +1,17 @@
+﻿function agregarNuevaTareaAlListado() {
+    tareasListadoViewModel.tareas.push(tareaElementoListadoViewModel({ id: 0, titulo: 'qqqqq' }));
+}
+
+function manejarFocusoutTituloTarea(tarea) {
+    //debugger;
+    const titulo = tarea.titulo();
+    //const titulo = 'QUEMADO';
+    //si no escribio nada se le mostrara
+    if (!titulo) {
+        //removemso el ultimo elemento
+        tareasListadoViewModel.tareas.pop();
+        return;
+    }
+
+    tarea.id(1);
+}
