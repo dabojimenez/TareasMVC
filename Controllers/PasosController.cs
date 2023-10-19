bffc9d66284b4947d2fee54699b46a3a5ae3f44d
sjimenezprocelec <sjimenez@procelec.ec>
2023-10-18 20:42:53 -0500

    creacion de con controlador pasos, para el envio del paso a guardar en la base de datos. se crea el ui, para agregar la descripcion del paso y si esta o no realizado

---
+++ Controllers/PasosController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+using TareasMVC.Models;
+using TareasMVC.Servicios;
+
+namespace TareasMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasosController : Controller
+    {
+        private readonly ApplicationDbContextClass context;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public PasosController(ApplicationDbContextClass context, IServicioUsuarios servicioUsuarios)
+        {
+            this.context = context;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+        [HttpPost("{tareaId:int}")]
+        public async Task<ActionResult<Paso>> Post(int tareaId, [FromBody] PasoCrearDTO pasoCrearDTO)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var tarea = await context.Tareas.FirstOrDefaultAsync(t => t.Id == tareaId);
+
+            if (tarea is null)
+            {
+                return NotFound();
+            }
+
+            if (tarea.UsuarioCreacionId != usuarioId)
+            {
+                //el usuario no permiso para acceder a dicha tarea, para crear un paso
+                return Forbid();
+            }
+            //verificamos si existen pasos
+            var existenPasos = await context.Pasos.AnyAsync(p => p.TareaId == tareaId);
+            int ordenMayor = 0;
+            if (existenPasos)
+            {
+                ordenMayor = await context.Pasos.Where(p => p.TareaId == tareaId)
+                    .Select(p => p.Orden)
+                    .MaxAsync();//el valor maximo
+            }
+
+            var paso = new Paso();
+
+            paso.TareaId = tareaId;
+            paso.Orden = ordenMayor + 1;
+            paso.Descripcion = pasoCrearDTO.Descripcion;
+            paso.Realizado = pasoCrearDTO.realizado;
+
+            context.Add(paso);
+            await context.SaveChangesAsync();
+            //retornamos el paso de donde vamos a sacar el id del paso
+            return paso;
+        }
+
+    }
+}
---
+++ wwwroot/js/pasos.js
@@ -0,0 +1,12 @@
+﻿function manejarClickAgregarPaso() {
+    //si el paso es nuevo, no agregaremos dos de forma inmediata
+    const indice = tareEditarViewModel.pasos().findIndex(p => p.esNuevo());
+    //si es distinto de menos uno, es porque ya existe uno
+    if (indice !== -1) {
+        return;
+    }
+    tareEditarViewModel.pasos.push(new pasoViewModel({ modoEdicion: true, realizado: false }));
+    //seleccionamos el texarea, y si esta visible, se le colocara el focus
+    $("[name=txtPasoDescripcion]:visible").focus();
+}
+
