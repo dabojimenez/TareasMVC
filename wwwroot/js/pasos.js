9b53babe982345bca3750e2ed00d4b5b08f20c81
sjimenezprocelec <sjimenez@procelec.ec>
2023-10-18 21:13:42 -0500

    se agrega ui, para salvar el paso agregado por el usuario

--- wwwroot/js/pasos.js
+++ wwwroot/js/pasos.js
@@ -10,3 +10,56 @@
     $("[name=txtPasoDescripcion]:visible").focus();
 }
 
+//funcion para cancelar
+function manejarClickCancelarPaso(paso) {
+    if (paso.esNuevo()) {
+        //eliminamos el apso que nunca se creo
+        tareEditarViewModel.pasos.pop();
+    } else {
+
+    }
+}
+//funcion para slavar el paso
+async function manejarClickSalvarPaso(paso) {
+    paso.modoEdicion(false);
+    const esNuevo = paso.esNuevo();
+    const idTarea = tareEditarViewModel.id;
+    const data = obtenerCuerpoPeticionPaso(paso);
+
+    if (esNuevo) {
+        insertarPaso(paso, data, idTarea);
+    } else {
+        //actulizaremos
+    }
+}
+
+//funcion que creara el cuerpo en formato json, para neviar al controlador
+function obtenerCuerpoPeticionPaso(paso) {
+    return JSON.stringify(
+        {
+            descripcion: paso.descripcion(),
+            realizado: paso.realizado()
+        }
+    );
+}
+
+//funcion que realziara la peticion http, para insertar
+async function insertarPaso(paso, data, idTarea) {
+    const respuesta = await fetch(`${urlPasos}/${idTarea}`,
+        {
+            body: data,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    if (respuesta.ok) {
+        const json = await respuesta.json();
+        paso.id(json.id);
+    } else {
+        //manejaremos el error con el modal
+        manejoErrorApi(respuesta);
+    }
+}
