b98a0a1b72ba9d24dd2a34766034a47f9496730b
sjimenezprocelec <sjimenez@procelec.ec>
2023-10-20 14:27:03 -0500

    se crea la UI para subir un archivo adjunto

---
+++ wwwroot/js/archivos_adjuntos.js
@@ -0,0 +1,7 @@
+﻿// guardamos una referencia al arhivoTarea
+let inputArchivoTarea = document.getElementById('archivoATarea');
+
+function manejarClickAgregarArchivoAdjunto() {
+    //aun que el input type="file", este oculto vamos a pdoer interactuar ocn el mismo gracias a la referencia realizada
+    inputArchivoTarea.click();
+}
