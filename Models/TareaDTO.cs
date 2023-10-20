c7ee427d2283df7db50f678e0de7d385a6588c69
sjimenezprocelec <sjimenez@procelec.ec>
2023-10-20 10:22:50 -0500

    Obtencion de datos de conteo usando EF, y modificando automapper, para calcular los dos atributos adicionales

--- Models/TareaDTO.cs
+++ Models/TareaDTO.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Titulo { get; set; }
         //public string Descripcion { get; set; }
+        public string PasosRealizados { get; set; }
+        public string PasosTotal { get; set; }
     }
 }
--- Servicios/AutoMapperProfiles.cs
+++ Servicios/AutoMapperProfiles.cs
@@ -10,7 +10,17 @@ namespace TareasMVC.Servicios
         public AutoMapperProfiles()
         {
             //creamos los mapeados del orgin, hacia el destino
-            CreateMap<Tarea, TareaDTO>();
+            CreateMap<Tarea, TareaDTO>()
+                //para los atrivutos de TareaDTO (PasosRealizados,PasosTotal)
+                .ForMember( //para el miembro
+                dto => dto.PasosTotal,
+                //de donde vamos a sacar dicha data (ent) entidad, que en este caso nuestra entidad es Tarea
+                //MapFrom = mapear desde
+                ent => ent.MapFrom(x => x.Pasos.Count())
+                )
+                .ForMember(dto => dto.PasosRealizados, 
+                ent => 
+                    ent.MapFrom(x => x.Pasos.Where(p => p.Realizado).Count()));
         }
     }
 }
